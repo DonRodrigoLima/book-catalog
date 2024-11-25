427558955f1501a3b5fe83c5c6e193e6ea9a652e
Rodrigo Lima <donrodrigo1987@outlook.com>
2024-11-25 01:29:56 -0300

    validacao no metodo salvar livro

--- src/main/java/com/rodrigolima/bookcatalog/service/LivroService.java
+++ src/main/java/com/rodrigolima/bookcatalog/service/LivroService.java
@@ -1,7 +1,11 @@
 package com.rodrigolima.bookcatalog.service;
 
 import com.rodrigolima.bookcatalog.entity.Livro;
+import com.rodrigolima.bookcatalog.exception.AssuntoNotFoundException;
+import com.rodrigolima.bookcatalog.exception.AutorNotFoundException;
 import com.rodrigolima.bookcatalog.exception.LivroNotFoundException;
+import com.rodrigolima.bookcatalog.repository.AssuntoRepository;
+import com.rodrigolima.bookcatalog.repository.AutorRepository;
 import com.rodrigolima.bookcatalog.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +17,25 @@ import java.util.List;
 public class LivroService {
 
     private final LivroRepository livroRepository;
+    private final AutorRepository autorRepository;
+    private final AssuntoRepository assuntoRepository;
 
     public Livro salvar(Livro livro) {
+        // Validar autores
+        livro.getAutores().forEach(autor -> {
+            if (!autorRepository.existsById(autor.getId())) {
+                throw new AutorNotFoundException(autor.getId());
+            }
+        });
+
+        // Validar assuntos
+        livro.getAssuntos().forEach(assunto -> {
+            if (!assuntoRepository.existsById(assunto.getId())) {
+                throw new AssuntoNotFoundException(assunto.getId());
+            }
+        });
+
+        // Salvar o livro
         return livroRepository.save(livro);
     }
 
